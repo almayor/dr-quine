aa51aeaf3d85bdd38a0ed83763eecc06cb884957
Alex Mayorov <mayorovme@gmail.com>
2021-08-17 22:55:38 +0300

    Sully

---
+++ C/Grace/Grace.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#define PROG int main() { FILE* stream = fopen("Grace_kid.c", "w"); if (stream > 0) REPL(stream); }
+#define REPL(stream) fprintf(stream, SRCS, 10, SRCS, 34)
+#define SRCS "#include <stdio.h>%1$c#define PROG int main() { FILE* stream = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c); if (stream > 0) REPL(stream); }%1$c#define REPL(stream) fprintf(stream, SRCS, 10, SRCS, 34)%1$c#define SRCS %3$c%2$s%3$c%1$cPROG%1$c"
+PROG
