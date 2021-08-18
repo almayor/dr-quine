9f4dfa91b4bae4d8d29cf6715db49f5b3334add2
Alex Mayorov <mayorovme@gmail.com>
2021-08-18 23:16:09 +0300

    now Sully compiles Sully_0.c but doesn't execute it

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 int n = 5;
 int main(){
-char name[] = "Sully_X.c";
-char cmd[] = "cc Sully_X.c -o Sully_X && ./Sully_X";
-char* s = "#include <stdlib.h>%1$c#include <stdio.h>%1$cint n = %2$i;%1$cint main(){%1$cchar name[] = %3$cSully_X.c%3$c;%1$cchar cmd[] = %3$ccc Sully_X.c -o Sully_X && ./Sully_X%3$c;%1$cchar* s = %3$c%4$s%3$c;%1$cname[6] = '0' + n;%1$ccmd[9] = '0' + n;%1$ccmd[22] = '0' + n;%1$ccmd[35] = '0' + n;%1$cFILE* fl = fopen(name, %3$cw%3$c);%1$cif (fl > 0) fprintf(fl, s, 10, n - 1, 34, s);%1$cfflush(fl);%1$cif (n > 0) system(cmd);%1$c}%1$c";
-name[6] = '0' + n;
-cmd[9] = '0' + n;
-cmd[22] = '0' + n;
-cmd[35] = '0' + n;
+char *fname = "Sully_%1$i.c";
+char *fcmd = "cc Sully_%1$i.c -o Sully_%1$i && if [ %1$i -gt 0 ]; then ./Sully_%1$i; fi";
+char *finj = "#include <stdlib.h>%1$c#include <stdio.h>%1$cint n = %2$i;%1$cint main(){%1$cchar *fname = %3$c%4$s%3$c;%1$cchar *fcmd = %3$c%5$s%3$c;%1$cchar *finj = %3$c%6$s%3$c;%1$cchar name[100], cmd[100], inj[100];%1$csprintf(name, fname, n);%1$csprintf(cmd, fcmd, n);%1$cFILE* fl = fopen(name, %3$cw%3$c);%1$cif (fl > 0) fprintf(fl, finj, 10, n - 1, 34, fname, fcmd, finj);%1$cfflush(fl);%1$csystem(cmd);%1$c}%1$c";
+char name[100], cmd[100], inj[100];
+sprintf(name, fname, n);
+sprintf(cmd, fcmd, n);
 FILE* fl = fopen(name, "w");
-if (fl > 0) fprintf(fl, s, 10, n - 1, 34, s);
+if (fl > 0) fprintf(fl, finj, 10, n - 1, 34, fname, fcmd, finj);
 fflush(fl);
-if (n > 0) system(cmd);
+system(cmd);
 }
--- python/Sully/Sully.py
+++ python/Sully/Sully.py
@@ -3,8 +3,8 @@
 import os
 
 n = 5
-s = '#!/usr/bin/env python3\n\nimport os\n\nn = %d\ns = %s\nf = open("Sully_%%d.py" %% n, "w")\nf.write(s %% (n - 1, repr(s)))\nf.close()\nif n > 0: os.system("python3 Sully_%%d.py" %% n)'
+s = '#!/usr/bin/env python3\n\nimport os\n\nn = %d\ns = %s\nf = open("Sully_%%d.py" %% n, "w")\nf.write(s %% (n - 1, repr(s)))\nf.close()\nif n > 0: os.system("if [ %%d -gt 0 ]; then python3 Sully_%%d.py; fi" %% (n, n))'
 f = open("Sully_%d.py" % n, "w")
 f.write(s % (n - 1, repr(s)))
 f.close()
-if n > 0: os.system("python3 Sully_%d.py" % n)
+if n > 0: os.system("if [ %d -gt 0 ]; then python3 Sully_%d.py; fi" % (n, n))
